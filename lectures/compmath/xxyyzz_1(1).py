57c6463c0f7cf1807295c9b99d747e7188a50b44
radicalalpaca <leocollins511@gmail.com>
2020-10-02 17:49:10 +0100

    02/10/20

---
+++ lectures/compmath/xxyyzz_1(1).py
@@ -0,0 +1,38 @@
+# Template file for Week 1
+
+# Lines that start with '#' are comments and are ignored by Python.
+
+#Exercise 1.A
+#Write your answers in the line below the question number. Your answers should be enclosed in a print command, e.g. print(``6 September 1978'')
+
+#A(i)
+
+#A(ii)
+
+#A(iii)
+
+#A(iv)
+
+
+#Exercise 1.B
+#Your answers should be enclosed in a print command, e.g. print(list(range(10))). Do not touch the lines of code defining a,b,x,y,
+#but write your answers in the lines after them.
+
+#B(i)
+
+#B(ii)
+
+#B(iii)
+a = list(range(70,1030,3))
+
+#B(iv)
+b = list(range(-41,10))
+
+#B(v)
+
+
+#B(vi)
+x,y = [1,3,5],[2,4,6]
+
+#B(vii)
+
