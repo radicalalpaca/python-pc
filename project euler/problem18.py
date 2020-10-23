f64049499544b307e22b4c13c38f62f4bad4e25e
radical_alpaca <leocollins511@gmail.com>
2020-10-23 02:03:29 +0100

    22/10/2020

--- project euler/problem18.py
+++ project euler/problem18.py
@@ -7,12 +7,19 @@ url = "https://projecteuler.net/problem=18"
 def scrape():
     r = requests.get(url).text
     soup = BeautifulSoup(r, "html.parser")
+    triangle = []
+    for line in soup.find_all(class_="monospace center", limit=3)[-1].stripped_strings:
+        triangle.append([int(i) for i in line.split(" ")])
+    return triangle
 
 
-def path_sum():
-    triangle = [[3], [7, 4], [2, 4, 6], [8, 5, 9, 3]]
-    
+def path_sum(triangle):
+    triangle.reverse()
+    for i in range(len(triangle) - 1):
+        for j in range(len(triangle[i]) - 1):
+            triangle[i + 1][j] += max([triangle[i][j], triangle[i][j + 1]])
+    return triangle[-1]
 
 
 if __name__ == '__main__':
-    print(path_sum())
+    print(path_sum(scrape()))
