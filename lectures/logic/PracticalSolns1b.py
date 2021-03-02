9517df6e9db92de1ce3e6f4877e812080b09a9db
radicalalpaca <leocollins511@gmail.com>
2021-03-02 16:52:45 +0000

    2/03/2021

--- lectures/logic/PracticalSolns1b.py
+++ lectures/logic/PracticalSolns1b.py
@@ -1,80 +1,75 @@
-# -*- coding: utf-8 -*-
-"""
-MATH2042 Practial Solutions 1b:
-    Object Oriented Programming.
-
-Lecturer: Andrew Brooke-Taylor
-"""
-
 class ParseTreeNode:
-    """A class whose objects represent nodes in a parse tree for a formula of 
-    propositional logic.
-    
-    Data attributes should be among label, child, secondchild, parent,
-    and childrencount."""
-    
-    def __init__(self):
-        self.childrencount = 0
-        
-    def assignChild(self,givennode):
-        """Sets givennode to be the (first) child of self if there are
-        currently no children, or the secondchild otherwise.  Also
-        sets the parent of givennode to be the current node.  
-        Note: doesn't handle well cases where the user tries to assign further
-        children."""
+    """Class that represents a node in a parse tree for a propositional formula.
+
+    :param string symbol: Either a propositional variable, or one of three connectives:
+        NOT, OR, AND, IMPLY.
+    """
+
+    def __init__(self, symbol):
+        self.label = symbol
+        self.childrencount = 0  # Number of children. Either 0, 1 or 2.
+        self.parent = None  # Parent of node. Initially empty.
+        self.child = None  # Child of node. Initially empty.
+        self.secondchild = None  # Second child of node. Initially empty.
+
+    def assignchild(self, givennode):
+        """Assigns a given node as child. If node already has one child, then given node
+        is assigned as secondchild.
+
+        :param ParseTreeNode givennode: Node to be assigned as child
+        """
         if self.childrencount == 0:
+            # If current node has no children, then new node is assigned
+            # as first child.
             self.child = givennode
+            self.childrencount += 1  # The current node now has 1 child.
         else:
+            #  Else the new node is assigned as second child.
             self.secondchild = givennode
-        self.childrencount = self.childrencount + 1
-        givennode.parent = self
-    
-    def assignLabel(self,givenlabel):
-        """Sets the label of the node to be givenlabel."""
-        self.label = givenlabel
-                    
-    def Subformula(self):
-        """Returns the subformula at the present node, as defined in 
-        Example 2.2.8 of the lecture notes.""" 
-        # We'll use - for not, > for impication, v for or, and & for and.
+        givennode.parent = self  # Assigns the parent of the new node as current node.
+
+    def subformula(self):
+        """Returns the subformula of current node.
+
+        :return: Subformula of node
+        :rtype: string
+        """
         if self.label == '-':
-            return '(-'+self.child.Subformula()+')'
-        elif self.label == '&':
-            return '('+self.child.Subformula()+' & '+self.secondchild.Subformula()+')'
-        elif self.label == 'v':
-            return '('+self.child.Subformula()+' v '+self.secondchild.Subformula()+')'
-        elif self.label == '>':
-            return '('+self.child.Subformula()+' > '+self.secondchild.Subformula()+')'
+            # NOT label
+            return '(-' + self.child.subformula() + ')'
+        elif self.label == "v":
+            # OR label
+            return "(" + self.child.subformula() + " v " + self.secondchild.subformula() + ")"
+        elif self.label == "&":
+            # AND label
+            return "(" + self.child.subformula() + " & " + self.secondchild.subformula() + ")"
+        elif self.label == ">":
+            # material conditional label
+            return "(" + self.child.subformula() + " > " + self.secondchild.subformula() + ")"
         else:
-            return self.label # This is what we want in the case of a leaf
+            # leaf node
+            return self.label
 
 
-def ParseTree(formula):
-    """Given a formula in the format described in ParseTreeAlgorithm.pdf
-    (i.e. as per the lecture notes, but with an extra pair of parentheses 
-    around each propositional variable or constant), returns the root node
-    of the parse tree of the formula."""
-    
-    #The variable cn will denote the "current node".  
-    #We create a dummy node to start with.
-    cn = ParseTreeNode()
-    
-    for char in formula:
-        if char == '(':
-            newnode = ParseTreeNode()
-            cn.assignChild(newnode)
-            cn = newnode
-        elif char == ')':
-            cn = cn.parent
+def parsetree(formula):
+    """Parses plain text propositional formula into a parse tree. Formula
+    must have brackets around each variable.
+
+    :param string formula: Propositional formula in plain text.
+    :return: Returns root node of parse tree.
+    :rtype: ParseTreeNode
+    """
+    current_node = ParseTreeNode("")  # dummy node
+    for character in formula:
+        if character == "(":
+            # Creates empty node and assigns it as the child of the previous node.
+            new_node = ParseTreeNode("")
+            current_node.assignchild(new_node)
+            current_node = new_node
+        elif character == ")":
+            # Changes current node to its parent.
+            current_node = current_node.parent
         else:
-            cn.assignLabel(char)
-
-    #At the end of that for loop, cn will be back pointing at the dummy
-    #node above the real root node.
-    return cn.child
-
-
-    
-
-
-
+            # Assigns the current node the label of the character.
+            current_node.label = character
+    return current_node.child  # Loop ends on dummy node; returns child of dummy node.
--- lectures/logic/practical2.py
+++ lectures/logic/practical2.py
@@ -1,49 +1,54 @@
-class ParseTreeNode:
-    """A class whose objects represent nodes in a parse tree for a formula of
-    propositional logic.
-
-    Data attributes should be among label, child, secondchild, parent."""
-
-    def __init__(self, symbol):
-        # Initialise the node with the given label
-        self.label = symbol
-        self.childrencount = 0
-        self.parent = None
-
-    def assignchild(self, givennode):
-        if self.childrencount == 0:
-            self.child = givennode
-            self.childrencount += 1
-        else:
-            self.secondchild = givennode
-        givennode.parent = self
-
-    def subformula(self):  # Work in progress!
-        # We'll use the symbols - for not, > for implies, v for or,
-        # and & for and.
-        if self.label == '-':
-            return '(-' + self.child.subformula() + ')'
-        elif self.label == "v":
-            return "(" + self.child.subformula() + " v " + self.secondchild.subformula() + ")"
-        elif self.label == "&":
-            return "(" + self.child.subformula() + " & " + self.secondchild.subformula() + ")"
-        elif self.label == ">":
-            return "(" + self.child.subformula() + " > " + self.secondchild.subformula() + ")"
-        else:
-            return self.label  # This is what we want in the case of a leaf
-            # In other cases it's wrong, but at least it won't cause the
-            # program to crash while we're testing the code.
+variables = set()  # Set of variables which must be true if the formula is satisfiable.
 
 
-def parsetree(formula):
-    current_node = ParseTreeNode("")  # dummy node
-    for character in formula:
-        if character == "(":
-            new_node = ParseTreeNode("")
-            current_node.assignchild(new_node)
-            current_node = new_node
-        elif character == ")":
-            current_node = current_node.parent
-        else:
-            current_node.label = character
-    return current_node.child
+def is_satisfiable(formula):
+    """Takes a propositional formula in conjunctive normal form and returns
+    true if formula is satisfiable along with a list of literals satisfying
+    the formula, and false if formula is not satisfiable. Uses the DPLL
+    algorithm. Formulas are represented by lists; each variable is represented
+    by an integer 1, 2, 3, .... The negation of each variable is represented by
+    -1, -2, -3, .... Each conjunct is a list of integers.
+
+    :param list formula: Propositional formula in conjunctive normal form
+    :return: Tuple (Boolean, list)
+    :rtype: tuple
+    """
+    if len(formula) == 0:
+        #  If there are no conjuncts, then the formula is true.
+        true_variables = [variable for variable in variables if variable >= 1]
+        #  If negation is true, then the variable is false.
+        return True, true_variables
+    for i in range(len(formula)):
+        #  Iterates through each conjunct.
+        if len(formula[i]) == 0:
+            #  If a conjunct has no literals, then it is false.
+            return False
+        if any([-literal in formula[i] for literal in formula[i]]):
+            #  If a variable and its negation are both in a conjunct, then
+            #  the conjunct is true and can be cancelled.
+            formula.remove(formula[i])
+        if len(formula[i]) == 1:
+            #  Unit propagation
+            variables.add(formula[i][0])  # The literal must be true.
+            for conjunct in formula:
+                if -formula[i][0] in conjunct:
+                    #  Removes the negation from each conjunct.
+                    conjunct.remove(-formula[i][0])
+            formula.remove(formula[i])  # Removes the literal.
+            return is_satisfiable(formula)
+        for literal in formula[i]:
+            #  Iterates through each literal in the conjunct.
+            if all([-literal not in conjunct for conjunct in formula]) and literal in variables:
+                #  Pure literal elimination.
+                variables.add(literal)  # Assume the literal must be true.
+                for conjunct in formula:
+                    # Iterates through all conjuncts and cancels if it contains the literal
+                    if literal in conjunct:
+                        formula.remove(conjunct)
+                return is_satisfiable(formula)
+            for conjunct in formula:
+                if literal in conjunct and -literal not in variables:
+                    # Assumes literal is true and removes all conjuncts containing the literal.
+                    formula.remove(conjunct)
+                    variables.add(literal)  # Adds true literal to list of true variables.
+                    return is_satisfiable(formula)
